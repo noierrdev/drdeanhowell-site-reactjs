ec5d43f8787d3151264b80f1143481788db0d348
Vander Moleker <noierdev@proton.me>
2024-03-29 08:33:17 +0100

    add server and port

---
+++ server.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the 'build' folder
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Define a catch-all route to serve your React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
